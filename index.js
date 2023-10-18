527cd222526f354f289c8740528ee757b30eafdb
Nikita <74122411+nikitaSPB@users.noreply.github.com>
2023-10-18 23:26:07 +0300

    Besnik

---
+++ index.js
@@ -0,0 +1,12 @@
+// const arr = [1, 2, 3, [4, 5, [6, 7]]];
+
+// const arr2 = [5, 6, 7, 8];
+
+
+// const res = arr.concat(arr2);
+
+// const filterRes = arr.filter(a => a !== 2);
+// const sortRes = arr.sort((a, b) => b - a)
+// const res = arr.flat(999999999)
+
+console.log(res)
